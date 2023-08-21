dee9986945cbbd38cbc0410523cd6c4fb4b19f70
jonspandorf <jonspandorf@gmail.com>
2023-08-21 19:37:56 +0000

    changed api endpoints frontend

--- frontend/src/lib/api.jsx
+++ frontend/src/lib/api.jsx
@@ -3,7 +3,7 @@ import axios from 'axios'
 const url = process.env.REACT_APP_URL || 'http://localhost:3000/'
 export const onPublishScore = async (data) => {
 
-    const res = await axios.put(`${url}api/upload`, data)
+    const res = await axios.put(`/api/upload`, data)
     return res.data
 
 }
