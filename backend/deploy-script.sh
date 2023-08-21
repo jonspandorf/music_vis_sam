4bfd40b098ba24f4f143767f596eb657212f0370
jonspandorf <jonspandorf@gmail.com>
2023-08-21 18:42:51 +0000

    changed deploy script

--- backend/deploy-script.sh
+++ backend/deploy-script.sh
@@ -3,10 +3,10 @@
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin ${LAMBDA_ECR_REPO}
 
 sam build -u && 
-# sam package \
-#  --region ${AWS_REGION} \
-#  --output-template-file packaged-template.yaml \
-#  --image-repository ${LAMBDA_ECR_REPO} && 
+sam package \
+ --region ${AWS_REGION} \
+ --output-template-file packaged-template.yaml \
+ --image-repository ${LAMBDA_ECR_REPO} && 
 sam deploy \
  --stack-name ${STACK_NAME} \
  --capabilities CAPABILITY_IAM \
