e6e223c6a3bf2c5c3612b050fd7992885da47502
jonspandorf <jonspandorf@gmail.com>
2023-08-22 17:35:26 +0000

    removed url from frontend

--- frontend/src/lib/api.jsx
+++ frontend/src/lib/api.jsx
@@ -2,11 +2,11 @@ import axios from 'axios'
 
 // No URL Attached for CF
 
-const url = process.env.REACT_APP_API || 'http://localhost:3000/'
+#const url = process.env.REACT_APP_API || 'http://localhost:3000/'
 
 export const onPublishScore = async (data) => {
 
-    const res = await axios.put(`${url}api/upload`, data)
+    const res = await axios.put(`/api/upload`, data)
     return res.data
 
 }
